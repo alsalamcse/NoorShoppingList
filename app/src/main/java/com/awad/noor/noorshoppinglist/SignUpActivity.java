150f76ddee63e141ae5ba2d7c91488c9fd35df00
user <noor@danon.tzafonet.org.il>
2017-10-16 09:46:08 +0300

    jhjlkhkl

--- app/src/main/java/com/awad/noor/noorshoppinglist/SignUpActivity.java
+++ app/src/main/java/com/awad/noor/noorshoppinglist/SignUpActivity.java
@@ -1,9 +1,13 @@
 package com.awad.noor.noorshoppinglist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity
 {
@@ -13,6 +17,11 @@ public class SignUpActivity extends AppCompatActivity
     private EditText etRePassword;
     private EditText etPhone;
     private Button btnSave;
+    private FirebaseAuth auth;
+    private FirebaseUser firebaseUser;
+
+
+
 
 
     @Override
@@ -25,8 +34,30 @@ public class SignUpActivity extends AppCompatActivity
         etRePassword= (EditText)findViewById(R.id.etRePassword);
         etPhone= (EditText)findViewById(R.id.etPhone);
         btnSave= (Button)findViewById(R.id.btnSave);
+        auth = FirebaseAuth.getInstance();
+        firebaseUser = auth.getCurrentUser();
+        auth = FirebaseAuth.getInstance();
+        firebaseUser = auth.getCurrentUser();
 
+
+        auth = FirebaseAuth.getInstance();
+        firebaseUser = auth.getCurrentUser();
+        if (firebaseUser == null) {
+            // Not signed in, launch the Sign In activity
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+            return;
+        } else {
+            String userName = firebaseUser.getDisplayName();
+            if (firebaseUser.getPhotoUrl() != null) {
+                String   photoUrl = firebaseUser.getPhotoUrl().toString();
+            }
+        }
+
+    }
+        }
 
 
     }
+
 }
